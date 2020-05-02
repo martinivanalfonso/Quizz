ea5997868bafe2626bd3e0afc775fd79d02645da
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-05-02 17:22:43 -0300

    implemented logic and data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,119 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FlashcardList from "./components/flashcard-list.component";
+import axios from "axios";
 
 import "./App.css";
 
-const App: React.FC = () => {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
-  return <FlashcardList flashcards={flashcards} />;
-};
+interface APIResponse {
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2?",
-    answer: "4",
-    options: ["2", "3", "4", "5"],
-  },
-  {
-    id: 2,
-    question: "What is 2 + 6?",
-    answer: "8",
-    options: ["8", "9", "10", "11"],
-  },
-  {
-    id: 3,
-    question: "What is 2 + 4?",
-    answer: "6",
-    options: ["5", "6", "7", "8"],
-  },
-];
+interface Category {
+  id: string;
+  name: string;
+}
+
+const App: React.FC = () => {
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const categoryRef = useRef<HTMLSelectElement>(null);
+  const amountRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api_category.php")
+      .then((res) => setCategories(res.data.trivia_categories));
+  }, []);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((item: APIResponse, index: number) => {
+          const answer = item.correct_answer;
+          const options = [
+            ...item.incorrect_answers.map((a: string) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(item.question),
+            answer: decodeString(answer),
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  const decodeString = (str: string) => {
+    const textArea: HTMLTextAreaElement = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    axios.get("https://opentdb.com/api.php?amount=10", {
+      params: {
+        amount: amountRef.current?.value,
+        category: categoryRef.current?.value
+      }
+    }).then((res) => {
+      setFlashcards(
+        res.data.results.map((item: APIResponse, index: number) => {
+          const answer = item.correct_answer;
+          const options = [
+            ...item.incorrect_answers.map((a: string) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(item.question),
+            answer: decodeString(answer),
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  };
+
+  return (
+    <>
+      <form className="header" onSubmit={(event) => handleSubmit(event)}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select name="category" id="category" ref={categoryRef}>
+            {categories.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountRef}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn" type="submit">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+      ;
+    </>
+  );
+};
 
 export default App;
--- src/components/flashcard.component.tsx
+++ src/components/flashcard.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 interface Flashcard {
   id: number;
@@ -9,21 +9,48 @@ interface Flashcard {
 
 const FlashCard: React.FC<{ flashcard: Flashcard }> = ({ flashcard }) => {
   const [flip, setFlip] = useState(false);
+  const [height, setHeight] = useState(0);
+
+  const frontRef = useRef<HTMLDivElement>(null);
+  const backRef = useRef<HTMLDivElement>(null);
+
+  const calculateMaxHeight = () => {
+    const frontHeight: number =
+      frontRef.current?.getBoundingClientRect().height || 0;
+    const backHeight: number =
+      frontRef.current?.getBoundingClientRect().height || 0;
+    setHeight(Math.max(frontHeight, backHeight, 100));
+  }
+
+  useEffect(() => {
+   calculateMaxHeight()
+  }, [flashcard.question]);
+
+  useEffect(() => {
+   window.addEventListener('resize', calculateMaxHeight)
+   
+   return () => window.removeEventListener('resize', calculateMaxHeight)
+  }, []);
 
   return (
     <div
       onClick={() => setFlip(!flip)}
       className={`card ${flip ? "flip" : ""}`}
+      style={{ height: `${height > 100 ? `${height}px` : "100px"}` }}
     >
-      <div className="front">
-        {flashcard.question}
+      <div className="front" ref={frontRef}>
+        <h4>{flashcard.question}</h4>
         <div className="options">
           {flashcard.options.map((option, index) => {
-            return <div className="option" key={index}>{option}</div>;
+            return (
+              <div className="option" key={index}>{`${index}) ${option}`}</div>
+            );
           })}
         </div>
       </div>
-      <div className="back">{flashcard.answer}</div>
+      <div className="back" ref={backRef}>
+        {flashcard.answer}
+      </div>
     </div>
   );
 };
