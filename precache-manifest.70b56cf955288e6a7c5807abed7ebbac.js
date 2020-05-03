d04554cbe052704c5fc36d42a09993334389526f
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-05-03 20:08:59 -0300

    Updates

---
+++ precache-manifest.70b56cf955288e6a7c5807abed7ebbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e971c9208cfc23a8e6089f6fa52dbc",
+    "url": "/Quizz/index.html"
+  },
+  {
+    "revision": "208df676b1ddb7a3484d",
+    "url": "/Quizz/static/css/main.cc584e4b.chunk.css"
+  },
+  {
+    "revision": "97416b35dded067e8881",
+    "url": "/Quizz/static/js/2.9470c336.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/Quizz/static/js/2.9470c336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208df676b1ddb7a3484d",
+    "url": "/Quizz/static/js/main.3f2872e1.chunk.js"
+  },
+  {
+    "revision": "8481bcef2ed7b7d7e8d5",
+    "url": "/Quizz/static/js/runtime-main.cf18fc22.js"
+  }
+]);
